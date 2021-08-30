70c2789c6813aa78be05390752c4876aafa5de61
Hans Baier <hansfbaier@gmail.com>
2021-08-31 06:39:41 +0700

    fix deadlock in channels_to_usb_stream

--- gateware/channels_to_usb_stream.py
+++ gateware/channels_to_usb_stream.py
@@ -5,6 +5,8 @@ from nmigen_library.stream  import StreamInterface, connect_fifo_to_stream
 
 class ChannelsToUSBStream(Elaboratable):
     def __init__(self, max_nr_channels=2, sample_width=24, max_packet_size=512):
+        assert sample_width in [16, 24, 32]
+
         # parameters
         self._max_nr_channels = max_nr_channels
         self._channel_bits    = Shape.cast(range(max_nr_channels)).width
@@ -42,10 +44,11 @@ class ChannelsToUSBStream(Elaboratable):
         shift = 8 if self._sample_width == 24 else 0
 
         with m.If(out_fifo.w_rdy):
-            with m.FSM():
+            with m.FSM() as fsm:
                 with m.State("WAIT-FIRST"):
+                    # we have to accept data until we find a first channel sample
+                    m.d.comb += channel_ready.eq(1)
                     with m.If(channel_valid & (channel_stream.channel_no == 0)):
-                        m.d.comb += channel_ready.eq(1)
                         m.d.sync += [
                             current_sample.eq(channel_payload << shift),
                             current_channel.eq(0),
@@ -65,15 +68,16 @@ class ChannelsToUSBStream(Elaboratable):
                     with m.If(current_byte == last_byte):
                         with m.If(channel_valid):
                             m.d.comb += channel_ready.eq(1)
+
                             current_channel_next = (current_channel + 1)[:self._channel_bits]
-                            m.d.sync += current_channel.eq(current_channel_next),
+                            m.d.sync += current_channel.eq(current_channel_next)
+
                             with m.If(current_channel_next == channel_stream.channel_no):
-                                m.d.sync += [
-                                    current_sample.eq(channel_payload << shift),
-                                ]
+                                m.d.sync += current_sample.eq(channel_payload << shift)
                                 m.next = "SEND"
                             with m.Else():
                                 m.next = "FILL-ZEROS"
+
                         with m.Else():
                             m.next = "WAIT"
 
